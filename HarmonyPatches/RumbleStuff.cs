90419b77ddfa26203d5cf213cf9989f0db93a798
Kinsi <kinsi55@users.noreply.github.com>
2021-04-13 03:35:23 +0200

    0.0.8

--- HarmonyPatches/RumbleStuff.cs
+++ HarmonyPatches/RumbleStuff.cs
@@ -5,17 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Tweaks55.HarmonyPatches {
-	[HarmonyPatch(typeof(NoteCutHapticEffect), MethodType.Constructor)]
+	[HarmonyPatch(typeof(NoteCutHapticEffect), nameof(NoteCutHapticEffect.HitNote))]
 	class RumbleStuff {
-		[HarmonyPriority(int.MaxValue)]
-		static void Postfix(HapticPresetSO ____rumblePreset) {
-			if(____rumblePreset == null || !Configuration.PluginConfig.Instance.enableCustomRumble)
-				return;
+		static HapticPresetSO _ourPreset = ScriptableObject.CreateInstance<HapticPresetSO>();
 
-			____rumblePreset._duration = Configuration.PluginConfig.Instance.cutRumbleDuration;
-			____rumblePreset._strength = Configuration.PluginConfig.Instance.cutRumbleStrength;
+		[HarmonyPriority(int.MinValue)]
+		static bool Prefix(ref HapticPresetSO ____rumblePreset) {
+			if(!Configuration.PluginConfig.Instance.enableCustomRumble)
+				return true;
+
+			_ourPreset._duration = Configuration.PluginConfig.Instance.cutRumbleDuration;
+			_ourPreset._strength = Configuration.PluginConfig.Instance.cutRumbleStrength;
+
+			if(_ourPreset._duration == 0f || _ourPreset._strength == 0f)
+				return false;
+
+			if(____rumblePreset == _ourPreset)
+				return true;
+
+			____rumblePreset = _ourPreset;
+
+			return true;
 		}
 	}
 }
