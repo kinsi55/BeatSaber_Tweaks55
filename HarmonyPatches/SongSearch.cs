31da4e2e1c6617ca7774fdf59249e7d79b8e13e3
Kinsi <kinsi55@users.noreply.github.com>
2021-07-04 13:26:42 +0200

    Heavily simplify this because all the IL stuff really isnt worth it here lol

--- HarmonyPatches/SongSearch.cs
+++ HarmonyPatches/SongSearch.cs
@@ -9,46 +9,14 @@ using System.Threading.Tasks;
 
 namespace Tweaks55.HarmonyPatches {
 
-	[HarmonyPatch]
+	[HarmonyPatch(typeof(BeatmapLevelFilterModel), "LevelContainsText")]
 	static class BasegameSearchFulltext {
-		static bool isActive = true;
-
-		[HarmonyPriority(int.MaxValue)]
-		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il) {
-			if(!ILUtil.CheckIL(instructions, new Dictionary<int, OpCode>() {
-				{93, OpCodes.Ldarg_0},
-				{96, OpCodes.Callvirt},
-				{104, OpCodes.Stfld}
-			})) {
-				Plugin.Log.Warn("basegameFulltextSearch: BeatmapLevelFilterModel.FilerBeatmapLevelPackCollectionAsync was not what I expected, not patching it");
-				isActive = false;
-				return instructions;
-			}
-
-			var conditional = new CodeInstruction[] {
-				new CodeInstruction(OpCodes.Ldc_I4_1),
-				new CodeInstruction(OpCodes.Newarr, typeof(string)),
-				new CodeInstruction(OpCodes.Dup),
-				new CodeInstruction(OpCodes.Ldc_I4_0)
-			}.ToList();
-
-			conditional.AddRange(instructions.ToList().GetRange(93, 4).Select(x => new CodeInstruction(x.opcode, x.operand)));
-
-			conditional.AddRange(new CodeInstruction[] {
-				new CodeInstruction(OpCodes.Stelem_Ref),
-
-				new CodeInstruction(OpCodes.Br, ILUtil.GetLabelForIndex(104, instructions, il))
-			});
-
-			ILUtil.InsertConditionalIL(93, ref instructions, il, AccessTools.Method(typeof(BasegameSearchFulltext), nameof(__EnableFulltextSearch)), conditional);
-
-			instructions.ElementAt(90).operand = ILUtil.GetLabelForIndex(93, instructions, il);
-
-			return instructions;
+		// Minvalue so other mods can still have a list of all the seperate words
+		[HarmonyPriority(int.MinValue)]
+		static void Prefix(ref string[] searchTexts) {
+			if(Configuration.PluginConfig.Instance.basegameFulltextSearch)
+				searchTexts = new string[] { string.Join(" ", searchTexts) };
 		}
-		static bool __EnableFulltextSearch() => Configuration.PluginConfig.Instance.basegameFulltextSearch;
-
-		static MethodBase TargetMethod() => ILUtil.GetIterator<BeatmapLevelFilterModel>("FilerBeatmapLevelPackCollectionAsync");
 
 		static Exception Cleanup(Exception ex) => Plugin.PatchFailed("BasegameSearchFulltext", ex);
 	}
