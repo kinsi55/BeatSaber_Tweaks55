ed8f2aa43b9bc6dc8339a162d5f3dc1f58102cc2
Kinsi <kinsi55@users.noreply.github.com>
2021-07-04 14:52:31 +0200

    Fix HMD only incompat

--- HarmonyPatches/BombColor.cs
+++ HarmonyPatches/BombColor.cs
@@ -11,8 +11,19 @@ namespace Tweaks55.HarmonyPatches {
 	static class BombColor {
 		[HarmonyPriority(int.MaxValue)]
 		static void Prefix(BombNoteController __instance) {
-			if(Configuration.PluginConfig.Instance.bombColor != Color.black)
-				__instance.transform.GetChild(0)?.GetComponent<Renderer>()?.material?.SetColor("_SimpleColor", Configuration.PluginConfig.Instance.bombColor);
+			if(Configuration.PluginConfig.Instance.bombColor == Color.black)
+				return;
+
+			var c = __instance.transform.GetChild(0);
+
+			if(c == null)
+				return;
+
+			// If CustomNotes "HMD Only" is active, there will be another nested child (The HMD bomb), which we should apply the color to instead
+			if(c.childCount != 0)
+				c = c.GetChild(0);
+
+			c.GetComponent<Renderer>()?.material?.SetColor("_SimpleColor", Configuration.PluginConfig.Instance.bombColor);
 		}
 
 		static Exception Cleanup(Exception ex) => Plugin.PatchFailed("BombColor", ex);
