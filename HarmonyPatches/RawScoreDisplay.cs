3de857730509caf8de41c6fe573515777a5c39d6
Kinsi <kinsi55@users.noreply.github.com>
2021-12-14 08:45:49 +0100

    Add option to hide Raw Score

---
+++ HarmonyPatches/RawScoreDisplay.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+
+namespace Tweaks55.HarmonyPatches {
+	[HarmonyPatch(typeof(ScoreUIController), nameof(ScoreUIController.UpdateScore))]
+	static class RawScoreDisplay {
+		[HarmonyPriority(int.MaxValue)]
+		static bool Prefix(TextMeshProUGUI ____scoreText) {
+			if(!Config.Instance.disableRawScore)
+				return true;
+
+			____scoreText.text = "";
+			return false;
+		}
+
+		static Exception Cleanup(Exception ex) => Plugin.PatchFailed("RawScoreDisplay", ex);
+	}
+}
