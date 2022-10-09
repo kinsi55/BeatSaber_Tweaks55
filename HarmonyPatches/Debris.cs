9fc7685d50b8fb789ccd26aa8aef55f6c2b61c2f
Kinsi <kinsi55@users.noreply.github.com>
2022-10-09 20:13:30 +0200

    Dont instantiate debris if debris is disabled

--- HarmonyPatches/Debris.cs
+++ HarmonyPatches/Debris.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using Tweaks55.Util;
 
 namespace Tweaks55.HarmonyPatches {
@@ -10,6 +12,34 @@ namespace Tweaks55.HarmonyPatches {
 		static bool Prefix() => !Config.Instance.disableDebris;
 
 		static MethodBase TargetMethod() => Resolver.GetMethod(nameof(NoteDebrisSpawner), nameof(NoteDebrisSpawner.SpawnDebris));
+		static Exception Cleanup(Exception ex) => Plugin.PatchFailed(ex);
+	}
+
+	// Game will pre-create 40 debris objects - If debris is disabled, we might as well not do that.
+	[HarmonyPatch]
+	static class DebrisPrefab {
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			var x = new CodeMatcher(instructions);
+			var m = new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DebrisPrefab), nameof(NoInitialSizeIfDebrisDisabled)));
+
+			x.MatchForward(true,
+				new CodeMatch(OpCodes.Ldc_I4_S),
+				new CodeMatch(x => (x.operand as MethodInfo)?.Name == "WithInitialSize")
+			).Repeat(x => {
+				x.InsertAndAdvance(m);
+			});
+
+			return x.InstructionEnumeration();
+		}
+
+		static int NoInitialSizeIfDebrisDisabled(int inSize) {
+			if(Config.Instance.disableDebris)
+				return 0;
+
+			return inSize;
+		}
+
+		static MethodBase TargetMethod() => Resolver.GetMethod(nameof(NoteDebrisPoolInstaller), "InstallBindings");
 		static Exception Cleanup(Exception ex) => Plugin.PatchFailed(ex);
 	}
 }
